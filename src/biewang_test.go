82ece1377f5f15eb95dbd89d1ec2dfdd8b4e0d67
princehaku <baizhongwei@163.com>
2013-07-14 09:44:04 +0800

    unit testing added

---
+++ src/biewang_test.go
@@ -0,0 +1,21 @@
+package biewang_test
+
+import (
+	"biewang"
+	"testing"
+	"fmt"
+)
+
+func TestMemo(t *testing.T) {
+	biewang.Str2Memo("明天的下午三点 去游戏")
+}
+
+func TestCNumber2Int(t *testing.T) {
+	 //c:= biewang.CnStr2Int("三亿九千八百一十七万二千一百五十四")
+}
+
+func BenchmarkCNumber2Int(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+		biewang.CnStr2Int("三亿九千八百一十七万二千一百五十四")
+    }
+}
