65f9f616dc3a89b367eb287291546b6d26d394cb
princehaku <baizhongwei@gmail.com>
2013-07-16 09:50:29 +0800

    还是要用旧的目录结构.

--- src/libbiewang/numberconv.go
+++ src/libbiewang/numberconv.go
@@ -1,4 +1,4 @@
-package biewang
+package libbiewang
 
 import (
 	"regexp"
--- src/main.go
+++ src/main.go
@@ -1,9 +1,10 @@
-package biewang
+package main
 
 import (
+	"libbiewang"
 )
 
 func main() {
-	Str2Memo("一小时二十三分钟一百零九十七秒以后")
-	Str2Memo("明天下午三点半")
+	libbiewang.Str2Memo("一小时二十三分钟一百零九十七秒以后")
+	libbiewang.Str2Memo("明天下午三点半")
 }
