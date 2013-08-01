126fb657d153f33c2fa0ffebacf7c62489829d4d
princehaku <baizhongwei@163.com>
2013-08-02 00:55:04 +0800

    更新测试脚本

--- src/biewang_test.go
+++ src/biewang_test.go
@@ -1,21 +1,30 @@
-package biewang_test
+package libbiewang_test
 
 import (
-	"biewang"
+	"github.com/princehaku/libbiewang"
 	"testing"
 	"fmt"
 )
 
-func TestMemo(t *testing.T) {
-	biewang.Str2Memo("明天下午三点")
+func TestStr2Memo(t *testing.T) {
+	pTime := libbiewang.Str2Memo("一个月三天一小时二十三分钟一百零九十七秒以后")
+	fmt.Println(pTime)
+	fmt.Println(pTime.Time())
 }
 
 func TestCNumber2Int(t *testing.T) {
-	 //c:= biewang.CnStr2Int("三亿九千八百一十七万二千一百五十四")
+	 c:= libbiewang.CnStr2Int("三亿九千八百一十七万二千一百五十四")
+	 fmt.Println(c)
+}
+
+func BenchmarkStr2Memo(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+	libbiewang.Str2Memo("一个月三天一小时二十三分钟一百零九十七秒以后")
+    }
 }
 
 func BenchmarkCNumber2Int(b *testing.B) {
     for i := 0; i < b.N; i++ {
-		biewang.CnStr2Int("三亿九千八百一十七万二千一百五十四")
+		libbiewang.CnStr2Int("三亿九千八百一十七万二千一百五十四")
     }
 }
